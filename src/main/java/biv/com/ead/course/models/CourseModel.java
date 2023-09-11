19ea69025184d5b493d3c278468cf8663a94baf3
bruniriz <bruna.iriz@gmail.com>
2023-09-10 23:36:38 -0300

    defining in the models, at attribute level, the ISO8601 date standard in UTC

--- src/main/java/biv/com/ead/course/models/CourseModel.java
+++ src/main/java/biv/com/ead/course/models/CourseModel.java
@@ -41,10 +41,10 @@ public class CourseModel {
     @Enumerated(EnumType.STRING)
     private CourseLevel courseLevel;
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime lastUpdateDate;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
